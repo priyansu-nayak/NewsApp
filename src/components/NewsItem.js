216765d4f7bb56fa39f4f329850f60f9814e6490
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-10 18:13:11 +0530

    updated

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
 
     render() {
-        let {title,description,imageUrl}=this.props;
+        let {title,description,imageUrl,newsUrl}=this.props;
         
         return (
             <div className='my-3'>
