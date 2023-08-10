913799748f517119efa216a4935d1830e813468c
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-10 17:14:09 +0530

    updated

--- src/components/News.js
+++ src/components/News.js
@@ -2,19 +2,26 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-  render() {
-    return (
-      <div>
-        This is a news component
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className='container my-3 '>
+                <h2>NewsMonkey- Top Headlines</h2>
+                
+                <div className="row flex " >
+                    <NewsItem title="myTitle " description="Kya baat hai bhai" />
+                    <NewsItem title=" " />
+                    <NewsItem title=" " />
+                </div>
+                
+                <div className="row flex" >
+                    <NewsItem title=" " />
+                    <NewsItem title=" " />
+                    <NewsItem title=" " />
+                </div>
+            
+            </div>
+        )
+    }
 }
 
 export default News
