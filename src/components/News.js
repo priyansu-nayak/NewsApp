cb93dc7fa490433b1331a21c6f0727580369a4d9
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-10 17:17:04 +0530

    updated

--- src/components/News.js
+++ src/components/News.js
@@ -6,19 +6,26 @@ export class News extends Component {
         return (
             <div className='container my-3 '>
                 <h2>NewsMonkey- Top Headlines</h2>
-                
-                <div className="row flex " >
-                    <NewsItem title="myTitle " description="Kya baat hai bhai" />
+
+                <div className="row" >
+                    <div className="col-md-3">
+                        <NewsItem title="myTitle " description="Kya baat hai bhai" />
+                    </div>
+                    <div className="col-md-3">
+                        <NewsItem title=" " />
+                    </div>
+                    <div className="col-md-3">
+                        <NewsItem title=" " />
+                    </div>
+
+                </div>
+
+                <div className="row" >
+                    <NewsItem title=" " />
                     <NewsItem title=" " />
                     <NewsItem title=" " />
                 </div>
-                
-                <div className="row flex" >
-                    <NewsItem title=" " />
-                    <NewsItem title=" " />
-                    <NewsItem title=" " />
-                </div>
-            
+
             </div>
         )
     }
