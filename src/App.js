f86c58b1735610e7e85bca892311eb4d614e8a96
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-15 13:54:26 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default class App extends Component {
   c = 'John';
   render() {
     return (
       <>
-       <Navbar/>
-       <News pageSize={6} country="us" category="business" />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={ <News key='general' pageSize={12} country='us' category='general' /> }></Route>
+
+            <Route exact path='/business' element={  <News key='business' pageSize={12} country='us' category='business' /> }></Route>
+
+            <Route exact path='/entertainment' element={  <News key='entertainment' pageSize={12} country='us' category='entertainment' /> }></Route>
+
+            <Route exact path='/health' element={  <News key='health' pageSize={12} country='us' category='health' /> }></Route>
+
+            <Route exact path='/science' element={  <News key='science' pageSize={12} country='us' category='science' /> }></Route>
+
+            <Route exact path='/sports' element={  <News key='sports' pageSize={12} country='us' category='sports' /> }></Route>
+
+            <Route exact path='/technology' element={  <News key='technology' pageSize={12} country='us' category='technology' /> }></Route>
+
+          </Routes>
+        </Router>
       </>
     )
   }
