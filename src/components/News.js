652f0501f6abeddd4f8acb8f9e47dae6f1fdfe9a
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-15 12:41:27 +0530

    updated

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
+    
+    static defaultProps={
+        country:'in',
+        pageSize:6
+    }
+
+    static propTypes={
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+    }
 
     /*
     articles = [
