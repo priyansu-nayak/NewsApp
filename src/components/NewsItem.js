fae0baff3fe36f8227e5976e6c2fa55e9363bf68
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-16 23:54:01 +0530

    updated

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
 
     render() {
-        let {title,description,imageUrl,newsUrl}=this.props;
+        let {title,description,imageUrl,newsUrl,author,date}=this.props;
         
         return (
             <div className='my-3'>
@@ -12,9 +12,10 @@ export class NewsItem extends Component {
                         <div className="card-body">
                             <h5 className="card-title">{title}...</h5>
                             <p className="card-text">{description}...</p>
+                            <p class="card-text"><small class="text-body-secondary">By {author} on {date}</small></p>
                             <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-dark">Read More</a>
                         </div>
-                </div>
+            </div>      
             </div>
         )
     }
