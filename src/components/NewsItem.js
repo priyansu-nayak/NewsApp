bd1617ff932854fe6fbf300da6f1370b3f1fc3e2
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-22 08:39:13 +0530

    updated

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,14 +4,22 @@ export class NewsItem extends Component {
 
     render() {
         let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
-        
+
         return (
             <div className='my-3'>
                 <div className="card" style={{ width: 18 + 'rem' }}  >
-                    <span className=" position-absolute top-0  translate-middle badge rounded-pill bg-primary" style={{zIndex:1, left:'90%' }}>
-                        {source}
-                        <span className="visually-hidden">unread messages</span>
-                    </span>
+
+                    <div>
+
+                        <span className="badge rounded-pill bg-primary" style={{ zIndex: 1, left: '90%' }}>
+                            {source}
+                            <span className="visually-hidden">unread messages</span>
+                        </span>
+
+                    </div>
+
+
+
                     <img src={imageUrl ? imageUrl : "https://imgs.search.brave.com/GTAN_r4ZPDDE03fFe56AJfzfBd3TgDaPvrqXIOqoaYo/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTM4/NTA4MjU3My92ZWN0/b3IvbGFwdG9wLWNv/bXB1dGVyLTQwNC1l/cnJvci1tZXNzYWdl/LmpwZz9zPTYxMng2/MTImdz0wJms9MjAm/Yz1vc0piMnR2dExo/QXBiUzF3WU1KTDBS/alZoZ2lYVEFDSjgx/Ry1WTkFmRThBPQ"} className="card-img-top" alt="..." height="150px" />
                     <div className="card-body">
                         <h5 className="card-title">{title}
