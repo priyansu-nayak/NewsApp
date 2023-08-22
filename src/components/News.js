1dc40cd63c6eb0f33c56e44ef232a9090d76cd07
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-22 13:41:50 +0530

    updated

--- src/components/News.js
+++ src/components/News.js
@@ -1,54 +1,14 @@
-import React, { Component } from 'react'
+import React ,{useEffect, useState} from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
 import InfiniteScroll from "react-infinite-scroll-component";
 
-export class News extends Component {
-
-    static defaultProps = {
-        country: 'in',
-        pageSize: 6,
-        category: 'general',
-    }
-
-    static propTypes = {
-        country: PropTypes.string,
-        pageSize: PropTypes.number,
-        category: PropTypes.string,
-    }
-
-    /*
-    articles = [
-        {
-            "source": {
-                "id": "bbc-sport",
-                "name": "BBC Sport"
-            },
-            "author": null,
-            "title": "Can cricketers play dodgeball?",
-            "description": "Watch as London Spirit and Oval Invincibles go head-to-head in an inaugural dodgeball match.",
-            "url": "http://www.bbc.co.uk/sport/av/cricket/66444760",
-            "urlToImage": "https://ichef.bbci.co.uk/news/1024/cpsprodpb/1049F/production/_130691766_p0g5jzy2.jpg",
-            "publishedAt": "2023-08-09T10:52:19.2374777Z",
-            "content": "Who will win the duel in the inaugural Hundred dodgeball match? Watch as London Spirit and Oval Invincibles take each other on to become dodgeball champion.\r\nREAD MORE: How are the men's and women's … [+71 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        }
-    ];
-    */
-
+const News =()=> {
+    const [articles,setArticles]=useState([]);
+    const [loading,setLoading]=useState(true);
+    const [page,setPage]=useState(1);
+    const [totalResults,setTotalResults]=useState(0);
 
     capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
@@ -217,6 +177,18 @@ export class News extends Component {
             </>
         )
     }
+}
+
+News.defaultProps = {
+    country: 'in',
+    pageSize: 6,
+    category: 'general',
+}
+
+News.propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
 }
 
 export default News
