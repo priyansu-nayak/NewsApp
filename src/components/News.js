22cb3086f4c293e5e1ea68be72f34b4b18078152
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-22 09:40:16 +0530

    updated

--- src/components/News.js
+++ src/components/News.js
@@ -72,8 +72,8 @@ export class News extends Component {
         
         this.props.setProgress(10);
         
-        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b10258dc32624b63b34b96b5e083de1f&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+//b10258dc32624b63b34b96b5e083de1f
         this.setState({
             loading: true
         })
