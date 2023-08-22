af2b7ca87d96e1f81b3f7847ff87204d331ec446
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-22 13:12:14 +0530

    updated

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 
-export class NewsItem extends Component {
+// export class NewsItem extends Component {
+const NewsItem = (props) => {
 
-    render() {
-        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = props;
 
         return (
             <div className='my-3'>
@@ -40,7 +40,7 @@ export class NewsItem extends Component {
                 </div>
             </div>
         )
-    }
+    
 }
 
 export default NewsItem
