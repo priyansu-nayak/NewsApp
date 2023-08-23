cb31d67914ec0d78f684a93ddba269a977925b53
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-23 16:05:37 +0530

    updated

--- src/components/News.js
+++ src/components/News.js
@@ -55,64 +55,18 @@ const News = (props) => {
 
     }
 
+    useEffect(() => {
+        first
 
+        return () => {
+            second
+        }
+    }, [third])
+    
 
     async componentDidMount() {
-
-        // let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=b10258dc32624b63b34b96b5e083de1f&page=1&pageSize=${props.pageSize}`;
-
-        // this.setState({
-        //     loading:true
-        // })
-
-        // let data = await fetch(url);
-
-        // let parsedData = await data.json()
-
-        // this.setState({
-        //      articles: parsedData.articles,
-        //      totalResults: parsedData.totalResults,
-        //      loading:false
-        //     })
         this.updateNews()
     }
-
-    /*
-        handlePrevClick = async () => {
-            // let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=b10258dc32624b63b34b96b5e083de1f&page=${this.state.page - 1}&pageSize=${props.pageSize}`;
-            // this.setState({loading:true})
-    
-            // let data = await fetch(url);
-            // let parsedData = await data.json()
-    
-            // this.setState({
-            //     articles: parsedData.articles,
-            //     page: this.state.page - 1,
-            //     loading:false
-            // })
-    
-            this.setState({ page: this.state.page - 1 })
-            this.updateNews()
-        }
-    
-        handleNextClick = async () => {
-            // if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / props.pageSize))) {
-    
-            //     let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=b10258dc32624b63b34b96b5e083de1f&page=${this.state.page + 1}&pageSize=${props.pageSize}`;
-            //     this.setState({ loading: true });
-            //     let data = await fetch(url);
-            //     let parsedData = await data.json()
-    
-            //     this.setState({
-            //         articles: parsedData.articles,
-            //         page: this.state.page + 1,
-            //         loading: false
-            //     });
-            // }
-            this.setState({ page: this.state.page + 1 })
-            this.updateNews()
-        }
-    */
 
 
     fetchMoreData = async () => {
@@ -133,49 +87,41 @@ const News = (props) => {
 
     };
 
-    render() {
+    return (
+        <>
+            <h1 className='text-center' style={{ margin: '35px 0px' }} >NewsMonkey- Top Headlines from {this.capitalizeFirstLetter(props.category)}</h1>
+            {/* {this.state.loading && <Spinner />} */}
 
-        return (
-            <>
-                <h1 className='text-center' style={{ margin: '35px 0px' }} >NewsMonkey- Top Headlines from {this.capitalizeFirstLetter(props.category)}</h1>
-                {/* {this.state.loading && <Spinner />} */}
+            <InfiniteScroll
+                dataLength={this.state.articles.length}
+                next={this.fetchMoreData}
+                hasMore={this.state.articles.length !== this.totalResults}
+                loader={this.state.loading && <Spinner />}
+            >
 
-                <InfiniteScroll
-                    dataLength={this.state.articles.length}
-                    next={this.fetchMoreData}
-                    hasMore={this.state.articles.length !== this.totalResults}
-                    loader={this.state.loading && <Spinner />}
-                >
+                <div className="container">
 
-                    <div className="container">
+                    <div className="row" >
+                        {!this.state.loading && this.state.articles.map((element, index) => {
+                            return (
+                                <div className="col-md-4" key={index}>
+                                    <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 80) : ""}
+                                        imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt}
+                                        source={element.source.name}
+                                    />
 
-                        <div className="row" >
-                            {!this.state.loading && this.state.articles.map((element, index) => {
-                                return (
-                                    <div className="col-md-4" key={index}>
-                                        <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 80) : ""}
-                                            imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt}
-                                            source={element.source.name}
-                                        />
-
-                                    </div>
-                                )
-                            })}
-                        </div>
-
+                                </div>
+                            )
+                        })}
                     </div>
 
-                </InfiniteScroll>
+                </div>
 
+            </InfiniteScroll>
+            
+        </>
+    )
 
-                {/* <div className="container d-flex justify-content-between">
-                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
-                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}> Next  &rarr;</button>
-                </div> */}
-
-            </>
-        )
-    }
 }
 
 News.defaultProps = {
